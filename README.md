d74809d75c169bb1ac0a6f2a8cd7070e432a8a36
Matt Carter <m@ttcarter.com>
2015-07-03 14:49:06 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+angular-bs-tooltip
+==================
+AngularJS + Bootstrap tooltip support.
+
+See the [ng-mfdc](http://momsfriendlydevco.github.io/ng-mfdc) showcase for demos.
---
+++ angular-bs-tooltip.js
@@ -0,0 +1,33 @@
+angular.module('angular-bs-tooltip', [])
+.directive('tooltip', function() {
+	return {
+		scope: {
+			tooltip: '@',
+			tooltipPosition: '@?',
+			tooltipContainer: '@?',
+			tooltipTrigger: '@?',
+			tooltipHtml: '@?'
+		},
+		restrict: 'A',
+		link: function($scope, elem) {
+			$scope.$watch('tooltip + tooltipPosition + tooltipContainer + tooltipTrigger', function() {
+				var isVisible = $(elem).siblings('.tooltip').length > 0; // Is the tooltip already shown?
+				if ($(elem).hasClass('ng-tooltip')) $(elem).tooltip('destroy');
+
+				$(elem)
+					.tooltip({
+						title: $scope.tooltip,
+						placement: $scope.tooltipPosition || 'top',
+						container: $scope.tooltipContainer || null,
+						trigger: $scope.tooltipTrigger || 'hover',
+						html: $scope.tooltipHtml || false,
+						animation: false
+					})
+					.addClass('ng-tooltip');
+
+				if (isVisible) // Reshow the tooltip if we WERE using it before
+					$(elem).tooltip('show');
+			});
+		}
+	}
+});

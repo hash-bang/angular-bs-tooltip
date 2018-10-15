67c6a1ffa9a2d62f332f28bd82182fa2640dfd36
Matt Carter <m@ttcarter.com>
2018-10-15 13:41:18 +1000

    BUGFIX: Compatibility with BS4 / Popper.js

--- src/angular-bs-tooltip.js
+++ src/angular-bs-tooltip.js
@@ -13,13 +13,22 @@ angular.module('angular-bs-tooltip', [])
 	controller: function($scope, $element, $interval) {
 		$scope.isVisible = false;
 
+		$scope.isBS3 = /^3/.test($.fn.tooltip.Constructor.VERSION);
+		$scope.isBS4 = /^4/.test($.fn.tooltip.Constructor.VERSION);
+
 		/**
 		* Rebuild the tooltip
 		*/
 		$scope.rebuild = ()=> {
 			var elem = $($element);
 			var isVisible = $scope.isVisible; // Local copy of the tooltip before we destroy it (or forced with tooltipShow)
-			if (elem.hasClass('ng-tooltip')) elem.tooltip('destroy');
+			if (elem.hasClass('ng-tooltip')) {
+				if ($scope.isBS3) {
+					elem.tooltip('destroy');
+				} else if ($scope.isBS4) {
+					elem.tooltip('dispose');
+				}
+			}
 
 			$scope.rebuildTether();
 
@@ -33,7 +42,10 @@ angular.module('angular-bs-tooltip', [])
 					trigger: ($scope.tooltipShow === true || $scope.tooltipShow === false) ? 'manual' : $scope.tooltipTrigger || 'hover',
 					html: !! $scope.tooltipHtml,
 					animation: false,
-					template: `<div class="tooltip ng-tooltip-${$scope.$id}" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>`,
+					template:
+						$scope.isBS3 ? `<div class="tooltip ng-tooltip-${$scope.$id}" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>`
+						: $scope.isBS4 ? `<div class="tooltip ng-tooltip-${$scope.$id}" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>`
+						: `<div class="tooltip ng-tooltip-${$scope.$id}" role="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-inner"></div></div>`,
 				})
 				.addClass('ng-tooltip');
 
@@ -48,36 +60,60 @@ angular.module('angular-bs-tooltip', [])
 		$scope.tetherTimer;
 
 		$scope.rebuildTether = ()=> {
-			if ($scope.tooltipTether) { // Try to tether this tooltip
+			if ($scope.tooltipTether) { // Try to tether this tooltip using tether.js
 				var elem = $($element)
 
 				elem.one('shown.bs.tooltip', ()=> { // Wait until the tooltip is visible then tether to it
-					$scope.tether = new Tether({
-						element: elem.data('bs.tooltip').$tip[0],
-						target: elem[0],
-						attachment:
-							$scope.tooltipPosition == 'top' ? 'bottom middle' :
-							$scope.tooltipPosition == 'left' ? 'center right' :
-							$scope.tooltipPosition == 'right' ? 'center left' :
-							$scope.tooltipPosition == 'bottom' ? 'top middle' :
-							'bottom middle'
-						,
-						targetAttachment:
-							$scope.tooltipPosition == 'top' ? 'top middle' :
-							$scope.tooltipPosition == 'left' ? 'center left' :
-							$scope.tooltipPosition == 'right' ? 'center right' :
-							$scope.tooltipPosition == 'bottom' ? 'bottom middle' :
-							'top middle'
-					});
+					if ($scope.isBS3) { // Attach BS3 / Tether to the element
+						$scope.tether = new Tether({
+							element: elem.data('bs.tooltip').$tip[0],
+							target: elem[0],
+							attachment:
+								$scope.tooltipPosition == 'top' ? 'bottom middle' :
+								$scope.tooltipPosition == 'left' ? 'center right' :
+								$scope.tooltipPosition == 'right' ? 'center left' :
+								$scope.tooltipPosition == 'bottom' ? 'top middle' :
+								'bottom middle'
+							,
+							targetAttachment:
+								$scope.tooltipPosition == 'top' ? 'top middle' :
+								$scope.tooltipPosition == 'left' ? 'center left' :
+								$scope.tooltipPosition == 'right' ? 'center right' :
+								$scope.tooltipPosition == 'bottom' ? 'bottom middle' :
+								'top middle'
+						});
 
-					// If its a number
-					if ($scope.tooltipTether === true || isFinite($scope.tooltipTether))
-						$scope.tetherTimer = $interval($scope.tether.position, $scope.tooltipTether === true ? 100 : $scope.tooltipTether);
+						if ($scope.tooltipTether === true || isFinite($scope.tooltipTether)) // Bind to refresh timer
+							$scope.tetherTimer = $interval($scope.tether.position, $scope.tooltipTether === true ? 100 : $scope.tooltipTether);
+					} else if ($scope.isBS4) {
+
+						// If its a number
+						if ($scope.tooltipTether === true || isFinite($scope.tooltipTether)) {
+							var lastPos;
+							var lastVisible;
+							$scope.tetherTimer = $interval(()=> {
+								var nowPos = elem.position();
+								var nowVisible = elem.is(':visible');
+
+								if (nowVisible && (lastPos === undefined || nowPos.left != lastPos.left || nowPos.top != lastPos.top)) {
+									elem.tooltip('update');
+									lastPos = nowPos;
+								}
+
+								if (lastVisible !== nowVisible) {
+									elem.tooltip(nowVisible ? 'show' : 'hide');
+									lastVisible = nowVisible;
+								}
+							}, $scope.tooltipTether === true ? 100 : $scope.tooltipTether);
+						}
+					}
 				});
 
 			} else if ($scope.tether) { // Destorying existing tether
-				$scope.tether.destroy();
-				$scope.tether = undefined;
+				if ($scope.isBS3) {
+					$scope.tether.destroy();
+					$scope.tether = undefined;
+				}
 				$interval.cancel($scope.tetherTimer);
 			}
 		};
